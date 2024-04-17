6bf18bb853446ab998bf29686da8897ef1ddd3b1
Steve NONO <stevelandryk89@gmail.com>
2024-04-17 11:28:36 +0200

    Gestion des annivs: Ajout et consultation des anniversaires

--- main.py
+++ main.py
@@ -16,12 +16,32 @@ def view_appointments():
     for appointment in sorted(appointments):
         print(f"{appointment[0].strftime('%d-%m-%Y %H:%M')} - {appointment[1]}")
 
+    """ ************ Gestion des anniversaires ************** """
+
+birthdays = []
+
+def add_birthday(date_str, name):
+    """Ajoute un anniversaire à la liste."""
+    date = datetime.strptime(date_str, "%d - %m")
+    birthdays.append((date, name))
+    print("Anniversaire ajouté avec succès !")
+
+def view_birthdays():
+    """Affiche les anniversaires à venir dans le mois courant."""
+    current_month = datetime.now().month
+    for birthday in birthdays:
+        if birthday[0].month == current_month:
+            print(f"{birthday[0].strftime('%d - %m')} - {birthday[1]}")
+
+
 def main():
     while True:
         print("\nAgenda Électronique")
         print("1. Ajouter un rendez-vous")
         print("2. Voir les rendez-vous")
-        print("3. Quitter")
+        print("3. Ajouter un anniversaire")
+        print("4. Voir les anniversaire")
+        print("5. Quitter")
         choice = input("Choisissez une option : ")
         if choice == '1':
             date = input("Entrez la date (JJ-MM-AAAA) : ")
@@ -31,6 +51,12 @@ def main():
         elif choice == '2':
             view_appointments()
         elif choice == '3':
+            date = input("Entrer la date (JJ-MM) : ")
+            name = input("Entrer le nom : ")
+            add_birthday(date, name)
+        elif choice == '4':
+            view_birthdays()
+        elif choice == '5':
             break
         else:
             print("Option non reconnue, veuillez réessayer.")
